00729f3f8207064f64f9c183af80497bc5533fa5
leticiamendes18 <145469245+leticiamendes18@users.noreply.github.com>
2025-05-09 18:28:34 -0300

    Add files via upload

---
+++ leticia-jogo--main/leticia-jogo--main/scrift.js
@@ -0,0 +1,27 @@
+const mario = document.querySelector('.mario');
+
+const jump = ()   {
+    mario.classlist.add('jump');
+
+    setTimeout(()  {
+
+        mario.classlist.remove('jump');
+
+    }, 500);
+}
+
+constlooppipeposition = pipe.offseleft;
+constmarioposition = +window.getComputedStyle;bottom.replace('px','');
+
+if (pipeposition  120 && pipeposition > 0 && marioposition < 88){}
+console.log(marioPosition);
+
+    if(pipeposition 120) {
+
+         pipe.style.animation = 'none'
+         pipe.style.left = '$(pipeposition}px';
+    }
+
+}, 10);   
+
+document.addEventListener('keydown', jump);
